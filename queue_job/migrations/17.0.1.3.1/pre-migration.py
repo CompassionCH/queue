a456d0867b6630b12c5c9224cad51d5d2be32c81
Emanuel Cino <ecino@compassion.ch>
2025-05-08 15:35:11 +0200

    FIX migrations for odoo.sh
    FIX migrations for odoo.sh

--- queue_job/migrations/17.0.1.3.1/pre-migration.py
+++ queue_job/migrations/17.0.1.3.1/pre-migration.py
@@ -1,10 +1,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 from openupgradelib import openupgrade
 
+from odoo import api, SUPERUSER_ID
 
-@openupgrade.migrate()
-def migrate(env, version):
+
+def migrate(cr, version):
     # Remove cron garbage collector
+    env = api.Environment(cr, SUPERUSER_ID, {})
     openupgrade.delete_records_safely_by_xml_id(
         env,
         ["queue_job.ir_cron_queue_job_garbage_collector"],
